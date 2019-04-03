d4c02cc5ae109a2021e6c216f34906cd1088c1c8
Ivan Chernoknizhnikov <chrnknzhnkv@gmail.comchrnknzhnkv@gmail.comchrnknzhnkv@gmail.comchrnknzhnkv@gmail.comchrnknzhnkv@gmail.com>
2019-04-03 20:02:27 +0300

    ADD checkDB function

--- main.py
+++ main.py
@@ -2,12 +2,157 @@ import mysql.connector
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 import time
+import datetime
+
+import threading
+from threading import Thread, Timer
+
+
+
+
 
 API_TOKEN = '830214956:AAFydska5qxwRXBxXgF6TbwYrEYAokGBHfE'
 
 bot = telebot.TeleBot(API_TOKEN)
 
 REFCOUNT = 10
+
+def printit():
+  threading.Timer(5.0, printit).start()
+  print("Hello, World!")
+  status = bot.get_chat_member("@nedocoder",	630184594)
+  print(status)
+
+#printit()
+
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+
+def DBUpdater():
+    #checkUser() # isActiveUser Status
+    checkTask() # Check Tasks
+
+
+timer = RepeatTimer(1, DBUpdater)
+timer.start()
+
+
+def checkTask():
+    dateNow = datetime.datetime.now()
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="root",
+        database="moneybot",
+        port="8889"
+    )
+    mycursor = mydb.cursor()
+    req = "SELECT \
+      users.id AS user, \
+      users.invitedCount as invCount, \
+      actions.userId AS task, \
+      actions.id as taskId, \
+      actions.channelName as channel, \
+      actions.actionDate as actiondDate, \
+      actions.status as actiondStatus \
+      FROM users \
+      INNER JOIN actions ON users.id = actions.userId AND users.invitedCount > 9 AND actions.status = 'pending'"
+    mycursor.execute(req)
+    tasks = mycursor.fetchall()
+    for task in tasks:
+        print(task)
+        isChannelMember = bot.get_chat_member(task[4], task[0]).status
+        actionDate = datetime.datetime.strptime(task[5], '%Y-%m-%d')
+        if isChannelMember == "member" and (dateNow - actionDate).days >= 4:
+            print("Юзер подписан и прошло 4 дня")
+            req_setComplete = "UPDATE actions SET status = 'complete' WHERE id = %s"
+            val_setComplete  = (task[3],)
+            mycursor.execute(req_setComplete, val_setComplete)
+            req_updateBalance = "UPDATE users SET balance = balance + 100 WHERE id = %s"
+            val_updateBalance = (task[0],)
+            mycursor.execute(req_updateBalance, val_updateBalance)
+            mydb.commit()
+
+
+
+
+
+def checkUser():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="root",
+        database="moneybot",
+        port="8889"
+    )
+    mycursor = mydb.cursor()
+    req = "SELECT * FROM users WHERE isActiveUser = 0 OR isActiveRef = 0"
+    mycursor.execute(req)
+    usersList = mycursor.fetchall()
+    for user in usersList:
+        print(user)
+        if user[5] == 0:
+            req_CheckTasks = "SELECT * FROM actions WHERE userId = %s AND status = 'complete'"
+            val_CheckTasks = (user[0],)
+            mycursor.execute(req_CheckTasks, val_CheckTasks)
+            taskList = mycursor.fetchall()
+            if len(taskList) >= 10:
+                setActiveUser(user[0],user[3])
+                print("User Is active", user[0])
+            else:
+                print("User is not active")
+
+
+
+def setActiveUser(userId, parentId):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="root",
+        database="moneybot",
+        port="8889"
+    )
+    mycursor = mydb.cursor()
+    sql_count = "UPDATE users SET isActiveUser = 1 WHERE id = %s"
+    val_count = (userId,)
+    mycursor.execute(sql_count, val_count)
+    if parentId != None:
+        print(parentId)
+        req_updateRefCount = "UPDATE users SET invitedCount = invitedCount + 1 WHERE id = %s"
+        val_updateRefCount = (parentId,)
+        mycursor.execute(req_updateRefCount, val_updateRefCount)
+    mydb.commit()
+    print(mycursor.rowcount, "User active updated")
+
+
+def updateUser(userId):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="root",
+        database="moneybot",
+        port="8889"
+    )
+    print(userId)
+    mycursor = mydb.cursor()
+    sql = "SELECT * FROM users WHERE invitedBy = %s"
+    val = (userId,)
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
+        if x[4] >= 10:
+            sql_count = "UPDATE users SET invitedCount = invitedCount + 1 WHERE id = %s"
+            val_count = (userId,)
+            mycursor.execute(sql_count, val_count)
+            mydb.commit()
+            print(mycursor.rowcount, "record inserted.")
+        else:
+            print("User invited < 10")
+
 
 def extract_unique_code(text):
     # Extracts the unique_code from the sent /start command.
@@ -40,6 +185,42 @@ def gen_markup_starter():
                InlineKeyboardButton("Вывод"))
     return markup
 
+def updateActiveBalance(actionId):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="root",
+        database="moneybot",
+        port="8889"
+    )
+    print("Update action",actionId)
+    mycursor = mydb.cursor()
+    sql = "UPDATE actions SET status = 'complete' WHERE id = %s"
+    val = (actionId,)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "record(s) affected")
+
+def getBalance(user):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="root",
+        database="moneybot",
+        port="8889"
+    )
+    mycursor = mydb.cursor()
+    sql = "SELECT * FROM actions WHERE userId = %s AND status = 'pending'"
+    val = (user.id,)
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    finalBalance = 0;
+    for x in myresult:
+        finalBalance += x[4]
+    print(finalBalance)
+    bot.send_message(user.id, "Баланс в ожидании: {0} руб\nДоступно к выводу: 0".format(finalBalance))
+
+
 
 def createUser(data):
     mydb = mysql.connector.connect(
@@ -66,7 +247,7 @@ def getChannel(userId, userName):
         port="8889"
     )
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM channels WHERE JSON_CONTAINS(`users`, JSON_OBJECT('user', %s)) = 0", (userId,))
+    mycursor.execute("SELECT * FROM channels WHERE currentAmount < finalAmount AND JSON_CONTAINS(`users`, JSON_OBJECT('user', %s)) = 0", (userId,))
 
     myresult = mycursor.fetchall()
     nowork = False
@@ -95,7 +276,8 @@ def getChannel(userId, userName):
 
 def checkCompleteSubscription(channelId, userId, lastMsg):
     isChannelMember = bot.get_chat_member(channelId, userId)
-    if isChannelMember:
+    print("Is Member", isChannelMember)
+    if isChannelMember.status == "member":
         print('member')
         updateChannelCount(channelId, userId, lastMsg)
     else:
@@ -115,14 +297,14 @@ def updateChannelCount(channelId, userId, lastMsg):
     print("Update Channel Count")
     mycursor = mydb.cursor()
     sql = "UPDATE channels SET currentAmount = currentAmount+1, users=JSON_ARRAY_APPEND(users, '$', JSON_OBJECT('user', %s,'date', %s)) WHERE channelName = %s"
-    sql_bal = "UPDATE users SET balance = balance+100 WHERE  id = %s"
+    #sql_bal = "UPDATE users SET balance = balance+100 WHERE  id = %s"
     sql_trans = "INSERT INTO actions(userId, channelName, actionDate, earned) VALUES (%s, %s, %s, %s)"
-    val_bal = (userId,)
+    #val_bal = (userId,)
     # sql = "UPDATE channels SET users=JSON_ARRAY_APPEND(users, '$', JSON_OBJECT('user', 'traemz','date', '2990-33-22')) WHERE channelName = '@nedocoder'"
     val = (userId, now, channelId)
     val_trans = (userId, channelId, now, 100)
     mycursor.execute(sql, val)
-    mycursor.execute(sql_bal, val_bal)
+    #mycursor.execute(sql_bal, val_bal)
     mycursor.execute(sql_trans, val_trans)
     bot.edit_message_text('Ты заработал 100 рублей', chat_id=userId, message_id=lastMsg, reply_markup=gen_markup_next())
     mydb.commit()
@@ -161,6 +343,8 @@ def callback_query(call):
         print(call.from_user.id)
     elif call.data == "cb_getNewAction":
         getChannel(call.from_user.id, call.from_user.username)  # get channel for subscribe
+    elif call.data == "cb_skip":
+        getChannel(call.from_user.id, call.from_user.username)  # get channel for subscribe
     elif call.data == "cb_rules":
         bot.answer_callback_query(call.id, "Click Rules")
     elif call.data == "cb_inviteFriends":
@@ -186,9 +370,16 @@ def send_welcome(message):
     mydb.commit()
     print(mycursor.rowcount, "record(s) affected")
 
+@bot.message_handler(commands=['checkref'])
+def check_activeRef(message):
+    print("Check Active Ref")
+    updateUser(message.chat.id)
 
-@bot.message_handler(commands=['test2'])
-def send_welcome(message):
+
+
+@bot.message_handler(commands=['updatebal'])
+def update_balance(message):
+    dateNow = datetime.datetime.now()
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -197,13 +388,43 @@ def send_welcome(message):
         port="8889"
     )
     mycursor = mydb.cursor()
-
-    mycursor.execute("SELECT * from `channels` WHERE JSON_CONTAINS(`users`, JSON_OBJECT('user', 'tr1emz'))")
-
+    sql = "SELECT * FROM actions WHERE userId = %s AND status = 'pending'"
+    val = (message.chat.id,)
+    mycursor.execute(sql, val)
     myresult = mycursor.fetchall()
-    print(myresult)
+    finalBalance = 0;
+
     for x in myresult:
         print(x)
+        actionDate = datetime.datetime.strptime(x[3], '%Y-%m-%d')
+        print("Сколько прошло дней", (dateNow - actionDate).days)
+        if((dateNow - actionDate).days >= 4):
+            updateActiveBalance(x[0])
+        else:
+            finalBalance += x[4]
+    print(finalBalance)
+    bot.send_message(message.chat.id, "Баланс в ожидании: {0} руб\nДоступно к выводу: 0".format(finalBalance))
+
+
+
+@bot.message_handler(commands=['test2'])
+def send_welcome(message):
+    DBUpdater()
+    # mydb = mysql.connector.connect(
+    #     host="localhost",
+    #     user="root",
+    #     passwd="root",
+    #     database="moneybot",
+    #     port="8889"
+    # )
+    # mycursor = mydb.cursor()
+    #
+    # mycursor.execute("SELECT * from `channels` WHERE JSON_CONTAINS(`users`, JSON_OBJECT('user', 'tr1emz'))")
+    #
+    # myresult = mycursor.fetchall()
+    # print(myresult)
+    # for x in myresult:
+    #     print(x)
 
 
 # Handle '/start' and '/help'
@@ -224,8 +445,11 @@ def send_welcome(message):
         )
         mycursor = mydb.cursor()
         sql = "UPDATE users SET invitedBy = %s WHERE id = %s"
+        #sql_count = "UPDATE users SET invitedCount = invitedCount + 1 WHERE id = %s"
+        #val_count = (unique_code,)
         val = (unique_code, message.chat.id)
         mycursor.execute(sql, val)
+        #mycursor.execute(sql_count, val_count)
         mydb.commit()
         print(mycursor.rowcount, "record inserted.")
 
@@ -241,6 +465,7 @@ def echo_message(message):
         getChannel(message.from_user.id, message.chat.username)  # get channel for subscribe
     elif message.text == 'Баланс':
         bot.send_message(message.chat.id, '{0}, твой баланс 0 руб'.format(message.chat.username))
+        getBalance(message.chat)
     elif message.text == 'Правила':
         bot.send_message(message.chat.id,
                          '❗{0}, с помощью этого бота можно хорошо заработать! Выполняйте задания и зарабатывайте деньги!'.format(
